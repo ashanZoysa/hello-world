4295f17155afcc416fb42595d1ceccf353fa81d7
ashanZoysa <it20037024@my.sliit.lk>
2022-02-28 23:00:44 +0530

    first commit

---
+++ HelloWorld/src/com/hello/main/Main.java
@@ -0,0 +1,10 @@
+package com.hello.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("This is my first project at SLIIT\n");
+	}
+
+}
